d4869dbe7e76e542139190dd4ec4b2b1ba4dda3c
Ravi Vishwakarma <raviv.vishwakarma@mediatech.co.in>
2021-10-18 17:50:52 +0530

    test

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 import routes from '../api';
 import config from '../config';
+var cors = require('cors');
 export default ({ app }: { app: express.Application }) => {
  
 
